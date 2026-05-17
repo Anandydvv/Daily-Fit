6ba0010bf22beefa417251e201fd0e60e477c713
Navteg Singh Bains <22103967@students.latrobe.edu.au>
2026-05-18 09:56:38 +1000

    feat: connect health feature screens to navigation

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,7 +2,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import DashboardScreen from "../screens/DashboardScreen";
+import GoalsScreen from "../screens/GoalsScreen";
+import LocationScreen from "../screens/LocationScreen";
 import LoginScreen from "../screens/LoginScreen";
+import ReminderScreen from "../screens/ReminderScreen";
 import SignupScreen from "../screens/SignupScreen";
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +17,9 @@ export default function AppNavigator() {
           name="Login"
           component={LoginScreen}
         />
+<Stack.Screen name="Goals" component={GoalsScreen} />
+<Stack.Screen name="Location" component={LocationScreen} />
+<Stack.Screen name="Reminder" component={ReminderScreen} />
 
         <Stack.Screen
           name="Signup"

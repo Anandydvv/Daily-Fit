6aa14411187cb745c71f26872d7a493bb49fd3b1
Navteg Singh Bains <22103967@students.latrobe.edu.au>
2026-05-18 11:42:11 +1000

    feat: create SQLite activity history storage

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,28 +2,34 @@ import * as SQLite from "expo-sqlite";
 
 const db = SQLite.openDatabaseSync("dailyfit.db");
 
-export const createTable = async () => {
+export const createActivityTable = async () => {
   await db.execAsync(`
-    CREATE TABLE IF NOT EXISTS goals (
+    CREATE TABLE IF NOT EXISTS activities (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
+      date TEXT,
+      startTime TEXT,
+      finishTime TEXT,
       steps INTEGER,
       calories INTEGER
     );
   `);
 };
 
-export const insertGoal = async (
+export const insertActivity = async (
+  date: string,
+  startTime: string,
+  finishTime: string,
   steps: number,
   calories: number
 ) => {
   await db.runAsync(
-    "INSERT INTO goals (steps, calories) VALUES (?, ?)",
-    [steps, calories]
+    `INSERT INTO activities 
+    (date, startTime, finishTime, steps, calories) 
+    VALUES (?, ?, ?, ?, ?)`,
+    [date, startTime, finishTime, steps, calories]
   );
 };
 
-export const getGoals = async () => {
-  return await db.getAllAsync(
-    "SELECT * FROM goals"
-  );
+export const getActivities = async () => {
+  return await db.getAllAsync("SELECT * FROM activities ORDER BY id DESC");
 };

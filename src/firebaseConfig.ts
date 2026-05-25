7f6c12fb259fe0050d7fb250ff5013d2d311965a
Anandydvv <anandinfo9899@gmail.com>
2026-05-25 10:22:01 +1000

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,14 +1,18 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "demo-api-key",
-  authDomain: "dailyfit.firebaseapp.com",
-  projectId: "dailyfit",
-  storageBucket: "dailyfit.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:demo",
+  apiKey: "AIzaSyCl-ZGDPl1vqT-eD7nnKbQPSbGXUKdwWpA",
+  authDomain: "dailyfit-474bc.firebaseapp.com",
+  projectId: "dailyfit-474bc",
+  storageBucket: "dailyfit-474bc.firebasestorage.app",
+  messagingSenderId: "609675830451",
+  appId: "1:609675830451:web:2f30b6e8fc2194c3763cc6",
+  measurementId: "G-RVP11QXHS9",
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
 
 export default app;
